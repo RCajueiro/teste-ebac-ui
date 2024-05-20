5efc22759994079299167ee56d73a1b7b745ec58
Ramon <ramoncajueiro@me.com>
2024-05-20 18:07:51 -0300

    Listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').first().click()
+        cy.get('.sku_wrapper').should('exist')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').last().click()
+        cy.get('.sku_wrapper').should('exist')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.sku_wrapper').should('exist')
+    });
+});
