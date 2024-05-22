8673d1a39a3750034a9a41e5578d54c33ef3ddd6
Ramon <ramoncajueiro@me.com>
2024-05-22 01:13:08 -0300

    Page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block').first().click()
+        produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('.sku_wrapper').should('exist')
     });
     it('Deve selecionar um produto da lista', () => {
@@ -17,5 +18,16 @@ describe('Funcionalidade: Produtos', () => {
     it('Deve selecionar um produto da lista', () => {
         cy.get('.product-block').contains('Ariel Roll Sleeve Sweatshirt').click()
         cy.get('.sku_wrapper').should('exist')
+    });
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.sold-text').should('exist')
+    });
+    it('Deve visitar a página do produto', () => {
+        
+    });
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
